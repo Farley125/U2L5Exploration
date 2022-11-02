03e509c8fa676ee64ad84236112e5c1f2c82084f
unknown <BT_4N2_03@BKTECH.LOCAL>
2022-11-02 12:34:40 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,52 @@
+public class Runner {
+    public static void main(String[] args) {
+        /*int i = 5;
+        while (i < 10)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        int x = 20;
+        while (x >= 0)
+        {
+            System.out.print(x + " ");
+            x -= 3;
+        }
+        System.out.println();
+        for (int z = 0; z <= 100; z += 5)
+        {
+            System.out.print(z + " ");
+        }
+        System.out.println();
+        for (int y = 10, j = 40; y <= j + 10; y += 5)
+        {
+            System.out.print(y + " ");
+        }
+
+
+        for (int x = 4; x < 16; x += 2)
+        {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        for (int y = 14, z = 10, a = 4; z >= 0; z -= 2, a = y - z)
+        {
+            System.out.print(a + " ");
+        }
+
+        for (int x = 6; x <= 100; x += 6)
+        {
+            System.out.print(x + " ");
+        }
+        */
+
+        int x = 10;
+        for (int i = 0; i < x; i++)
+        {
+            System.out.println(i);
+            x--;
+        }
+
+    }
+}
